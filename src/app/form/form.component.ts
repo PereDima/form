947556d603937b35bd6c083c8d520b1eb1fb08a2
Dmytro Pereklita <74322013+PereDima@users.noreply.github.com>
2021-10-01 00:13:57 +0300

    testing first forms

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  myForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.myForm.value);
   }
-
 }
