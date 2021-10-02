15e93786620207042f9e3c9d7ce73b20483dc639
Dmytro Pereklita <74322013+PereDima@users.noreply.github.com>
2021-10-02 15:52:52 +0300

    fixed  issues

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,70 +7,63 @@ import {
   Validators,
 } from '@angular/forms';
 
-
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-
   myForm: FormGroup | any;
   hide = true;
 
-  firstName = new FormControl('', [
-    Validators.required,
-    Validators.min(2)
-  ]);
-  lastName = new FormControl('', [
-    Validators.required,
-    Validators.min(3)
-  ]);
-  nickName = new FormControl('', [
-    Validators.required,
-  ]);
-  email = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  password = new FormControl('', [
-    Validators.required,
-    Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{8,}$')
-  ])
-
-  
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-
     this.myForm = new FormGroup({
-
       firstName: new FormControl('', [
         Validators.required,
-        Validators.minLength(2)
+        Validators.minLength(2),
       ]),
       lastName: new FormControl('', [
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(3),
       ]),
-      nickName: new FormControl('', [
-        Validators.required,
-      ]),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.email,
-      ]),
+      nickName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
         Validators.required,
-        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')
-      ])
-
-
+        Validators.pattern(
+          '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
+        ),
+      ]),
     });
   }
+
+  get firstName() {
+    return this.myForm.get('firstName') as FormArray;
+  }
+
+  get lastName() {
+    return this.myForm.get('lastName') as FormArray;
+  }
+
+  get nickName() {
+    return this.myForm.get('nickName') as FormArray;
+  }
+
+  get email() {
+    return this.myForm.get('email') as FormArray;
+  }
+
+  get password() {
+    return this.myForm.get('password') as FormArray;
+  }
+
   onSubmit() {
     if (this.myForm.valid) {
-      console.log(this.myForm.value);
+      const formData = { ...this.myForm.value };
+      console.log(formData);
+      this.myForm.reset();
     }
   }
 }
